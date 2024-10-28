9603b70ec65cb10ee993d569ab280ad6ccfa1c7d
TheJades <thejadesisfat77@gmail.com>
2024-10-28 17:48:51 -0600

    Updated Changes:  - Removed old irrelelvant code from the Repository Trees

--- consolescript.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void systemNormaLog(const char * message);
-void systemVerboseLog(const char * message);
-void systemWarningLog(const char * message);
-void systemErrorLog(const char * message);
